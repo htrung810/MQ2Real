d8250ded291f3438f2a6ff2c7e2f8c8e31895c5c
htrung810 <htrung810@gmail.com>
2022-11-04 11:53:09 +0700

    Up readme

--- CODE3/main.c
+++ CODE3/main.c
@@ -1,26 +1,3 @@
-/*******************************************************
-This program was created by the
-CodeWizardAVR V3.12 Advanced
-Automatic Program Generator
-© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
-http://www.hpinfotech.com
-
-Project : 
-Version : 
-Date    : 10/20/2022
-Author  : 
-Company : 
-Comments: 
-
-
-Chip type               : ATmega32L
-Program type            : Application
-AVR Core Clock frequency: 8.000000 MHz
-Memory model            : Small
-External RAM size       : 0
-Data Stack size         : 512
-*******************************************************/
-
 #include <main.h>
 
 void main(void)
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# MQ2Real
-Báo cháy sử dụng ATmega32L và simcom7670C
+# Báo cháy sử dụng ATmega32 kết hợp sim
+Thiết kế mạch điện tử + mô phỏng + source code.
+Phần mềm: Altium, Proteus, codevisionAVR.
+
+##PCB trên altium
+![img1](https://ibb.co/Ks19VWw)
+##Mạch đã hoàn thiện
+![img2](https://ibb.co/52W6Zk0)
+
+
+##Liên hệ
+Email: htrung810@gmail.com
+FB   : [htrung810](https://www.facebook.com/htrung810/)
+
+###Đây là thiết kế mạch báo cháy cơ bản.
+- Có thể vẽ lại PCB và thêm chức năng.
+- đã tạo 1 số jump đực để như 1 mạch phát triển.
+- Code đã khai báo ADC, có thể dùng để đọc giá trị của cảm biến.
